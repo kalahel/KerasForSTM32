9f9cf4e586b3f3ca866786391de02899136b06e0
kalahel <mathieu95480@gmail.com>
2021-02-18 18:35:33 +0100

    ✨ updated test script

--- checkModel.py
+++ checkModel.py
@@ -1,23 +1,14 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
 from keras.datasets import mnist
-from keras.layers import Flatten
-from keras.layers import Input
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Dense
-from keras.models import Model
-from keras.regularizers import l2
 from keras.utils import np_utils
-
-# Loading mnist dataset
 from tensorflow.python.keras.models import load_model
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-
 reconstructed_model = load_model("mnistSmall.h5")
-inference_result = reconstructed_model.predict(np.array([X_train[0] / 255, ]))
-print(inference_result)
+X_test = X_test.astype('float32')
+# Normalizing data
+X_test /= 255
+# one-hot encoding using keras numpy-related utilities
+n_classes = 10
+Y_test = np_utils.to_categorical(y_test, n_classes)
+results = reconstructed_model.evaluate(X_test, Y_test, batch_size=128)
+print("test loss, test acc:", results)
