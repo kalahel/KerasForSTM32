ad26b782073486c72cd278bf7da805a12b6b1571
kalahel <mathieu95480@gmail.com>
2021-02-18 14:44:47 +0100

    🐛 Fixed array copy, and removed useless code in C implementation

--- STM32/mnistSmallUsartV1/main.c
+++ STM32/mnistSmallUsartV1/main.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -65,242 +65,229 @@ static void MX_USART2_UART_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 	/* USER CODE BEGIN 1 */
 
-		/* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-		/* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-		/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-		HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-		/* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-		/* USER CODE END Init */
+	/* USER CODE END Init */
 
-		/* Configure the system clock */
-		SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-		/* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-		/* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-		/* Initialize all configured peripherals */
-		MX_GPIO_Init();
-		MX_CRC_Init();
-		MX_USART2_UART_Init();
-		MX_X_CUBE_AI_Init();
-		/* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_CRC_Init();
+	MX_USART2_UART_Init();
+	MX_X_CUBE_AI_Init();
+	/* USER CODE BEGIN 3 */
 
-		ai_handle mnist_model = AI_HANDLE_NULL;
+	ai_handle mnist_model = AI_HANDLE_NULL;
 
-		// Input/output buffer initialization
-		ai_buffer inputs[AI_MNISTSMALL_IN_NUM] = AI_MNISTSMALL_IN;
-		ai_buffer ouputs[AI_MNISTSMALL_OUT_NUM] = AI_MNISTSMALL_OUT;
+	// Input/output buffer initialization
+	ai_buffer inputs[AI_MNISTSMALL_IN_NUM] = AI_MNISTSMALL_IN;
+	ai_buffer ouputs[AI_MNISTSMALL_OUT_NUM] = AI_MNISTSMALL_OUT;
 
-		// Chunk of memory used to hold intermediate values for neural network
-		AI_ALIGNED(4) ai_u8 activations_buffer[AI_MNISTSMALL_DATA_ACTIVATIONS_SIZE];
+	// Chunk of memory used to hold intermediate values for neural network
+	AI_ALIGNED(4) ai_u8 activations_buffer[AI_MNISTSMALL_DATA_ACTIVATIONS_SIZE];
 
-		// Set working memory and get weights from model
-		ai_network_params params_model = {
-		AI_MNISTSMALL_DATA_WEIGHTS(ai_mnistsmall_data_weights_get()),
-		AI_MNISTSMALL_DATA_ACTIVATIONS(activations_buffer) };
+	// Set working memory and get weights from model
+	ai_network_params params_model = {
+	AI_MNISTSMALL_DATA_WEIGHTS(ai_mnistsmall_data_weights_get()),
+	AI_MNISTSMALL_DATA_ACTIVATIONS(activations_buffer) };
 
-		// Buffers used to store input and output tensors
-		AI_ALIGNED(4) ai_i8 in_data[AI_MNISTSMALL_IN_1_SIZE_BYTES];
-		AI_ALIGNED(4) ai_i8 out_data[AI_MNISTSMALL_OUT_1_SIZE_BYTES];
+	// Buffers used to store input and output tensors
+	AI_ALIGNED(4) ai_i8 in_data[AI_MNISTSMALL_IN_1_SIZE_BYTES];
+	AI_ALIGNED(4) ai_i8 out_data[AI_MNISTSMALL_OUT_1_SIZE_BYTES];
 
-		// Set pointers wrapper structs to our data buffers
-		inputs[0].n_batches = 1;
-		inputs[0].data = AI_HANDLE_PTR(in_data);
-		ouputs[0].n_batches = 1;
-		ouputs[0].data = AI_HANDLE_PTR(out_data);
+	// Set pointers wrapper structs to our data buffers
+	inputs[0].n_batches = 1;
+	inputs[0].data = AI_HANDLE_PTR(in_data);
+	ouputs[0].n_batches = 1;
+	ouputs[0].data = AI_HANDLE_PTR(out_data);
 
-		// -- MODEL DEFINITION AND INITIALIZATION --
+	// -- MODEL DEFINITION AND INITIALIZATION --
 
-		// Create instance of neural network
-		ai_error ai_err = ai_mnistsmall_create(&mnist_model,
-		AI_MNISTSMALL_DATA_CONFIG);
-		if (ai_err.type != AI_ERROR_NONE) {
-			// todo Handle error
-			printf("Error on model creation\r\n");
-		}
+	// Create instance of neural network
+	ai_error ai_err = ai_mnistsmall_create(&mnist_model,
+	AI_MNISTSMALL_DATA_CONFIG);
+	if (ai_err.type != AI_ERROR_NONE) {
+		// todo Handle error
+		printf("Error on model creation\r\n");
+	}
 
-		// Initialize neural network
-		if (!ai_mnistsmall_init(mnist_model, &params_model)) {
-			// todo handle error
-			printf("Error on model initialization\r\n");
-		}
+	// Initialize neural network
+	if (!ai_mnistsmall_init(mnist_model, &params_model)) {
+		// todo handle error
+		printf("Error on model initialization\r\n");
+	}
 
-		char buf_tx[AI_MNISTSMALL_OUT_1_SIZE_BYTES] = { 0 };
+	char buf_tx[AI_MNISTSMALL_OUT_1_SIZE_BYTES] = { 0 };
 
-		HAL_UART_Receive(&huart2, (uint8_t*) in_data, AI_MNISTSMALL_IN_1_SIZE_BYTES,
-				1000000);
-		// Computing and sending checksum of received data
-		float checksum = 0.0;
-		for (int i = 0; i < AI_MNISTSMALL_IN_1_SIZE; ++i) {
-			checksum += ((float*) in_data)[i];
-		}
-		HAL_UART_Transmit(&huart2, (uint8_t*) &checksum,
-			sizeof(float), 1000000);
-		ai_i32 nb_batches_treated = ai_mnistsmall_run(mnist_model, inputs, ouputs);
-		// WARNING RUNNING THE MODEL SEEMS TO RENDER ALL UART USAGE IMPOSSIBLE, NEED TO INVESTIGATE
+	HAL_UART_Receive(&huart2, (uint8_t*) in_data, AI_MNISTSMALL_IN_1_SIZE_BYTES,
+			1000000);
+	// Computing and sending checksum of received data
+	float checksum = 0.0;
+	for (int i = 0; i < AI_MNISTSMALL_IN_1_SIZE; ++i) {
+		checksum += ((float*) in_data)[i];
+	}
+	HAL_UART_Transmit(&huart2, (uint8_t*) &checksum, sizeof(float), 1000000);
 
-		float simple_outputs[AI_MNISTSMALL_OUT_1_SIZE] = { 0 };
-		for (int i = 0; i < AI_MNISTSMALL_OUT_1_SIZE; ++i) {
-			simple_outputs[i] = ((float*) ouputs->data)[i];
-		}
+	ai_i32 nb_batches_treated = ai_mnistsmall_run(mnist_model, inputs, ouputs);
 
-		float send_values[10] = { 2.2, 6.3, 4.54, 9.65, 1.2, 6.333, 7.6545, 88.22,
-				0.66, 0.48 };
-		for (int i = 0; i < 10 * sizeof(float); ++i) {
-			buf_tx[i] = ((char*) send_values)[i];
-		}
-		HAL_UART_Transmit(&huart2, (uint8_t*) buf_tx,
-		AI_MNISTSMALL_OUT_1_SIZE_BYTES, 1000000);
+	// This is used to visualize easily our outputs using the debugger
+	float simple_outputs[AI_MNISTSMALL_OUT_1_SIZE] = { 0 };
+	for (int i = 0; i < AI_MNISTSMALL_OUT_1_SIZE; ++i) {
+		simple_outputs[i] = ((float*) ouputs->data)[i];
+	}
 
-		return 0;
+	HAL_UART_Transmit(&huart2, (uint8_t*) simple_outputs,
+	AI_MNISTSMALL_OUT_1_SIZE_BYTES, 1000000);
+
+	return 0;
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 16;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 2;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief CRC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_CRC_Init(void)
-{
+ * @brief CRC Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_CRC_Init(void) {
 
-  /* USER CODE BEGIN CRC_Init 0 */
+	/* USER CODE BEGIN CRC_Init 0 */
 
-  /* USER CODE END CRC_Init 0 */
+	/* USER CODE END CRC_Init 0 */
 
-  /* USER CODE BEGIN CRC_Init 1 */
+	/* USER CODE BEGIN CRC_Init 1 */
 
-  /* USER CODE END CRC_Init 1 */
-  hcrc.Instance = CRC;
-  if (HAL_CRC_Init(&hcrc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CRC_Init 2 */
+	/* USER CODE END CRC_Init 1 */
+	hcrc.Instance = CRC;
+	if (HAL_CRC_Init(&hcrc) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN CRC_Init 2 */
 
-  /* USER CODE END CRC_Init 2 */
+	/* USER CODE END CRC_Init 2 */
 
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART2_UART_Init(void) {
 
-  /* USER CODE BEGIN USART2_Init 0 */
+	/* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART2_Init 0 */
+	/* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN USART2_Init 1 */
+	/* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
+	/* USER CODE END USART2_Init 1 */
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END USART2_Init 2 */
+	/* USER CODE END USART2_Init 2 */
 
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : B1_Pin */
+	GPIO_InitStruct.Pin = B1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : LD2_Pin */
+	GPIO_InitStruct.Pin = LD2_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
 }
 
@@ -309,18 +296,16 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
